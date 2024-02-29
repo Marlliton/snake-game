ae204a1ebae14fb8a65a29c4f4ce19c9f722762a
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2024-02-29 20:28:12 -0300

    improvement in "increaseBody" function logic

--- packages/core/src/domain/enterprise/entities/player/index.ts
+++ packages/core/src/domain/enterprise/entities/player/index.ts
@@ -9,7 +9,7 @@ type Body = {
   y: number;
 };
 
-type acceptedMoves = {
+type AcceptedMoves = {
   [key: string]: () => Player;
 };
 
@@ -49,32 +49,27 @@ export class Player extends Entity<Player, PlayerProps> {
   }
 
   increaseBody() {
+    const movementMap = {
+      up: { y: 1, x: 0 },
+      down: { y: -1, x: 0 },
+      left: { y: 0, x: 1 },
+      right: { y: 0, x: -1 },
+    };
     const lastCoordinate = this.fullBodyCoordinate.slice(-1)[0];
-    if (this.lastMovement === "up") {
-      const newBodyItem: Body = { y: lastCoordinate!.y + 1, x: lastCoordinate!.x };
+    const movement = movementMap[this.lastMovement!];
+    if (movement) {
+      const newBodyItem: Body = {
+        y: lastCoordinate!.y + movement.y,
+        x: lastCoordinate!.x + movement.x,
+      };
 
       return this.clone({ body: this.body.concat(newBodyItem) });
     }
-    if (this.lastMovement === "down") {
-      const newBodyItem: Body = { y: lastCoordinate!.y - 1, x: lastCoordinate!.x };
-
-      return this.clone({ body: this.body.concat(newBodyItem) });
-    }
-    if (this.lastMovement === "left") {
-      const newBodyItem: Body = { y: lastCoordinate!.y, x: lastCoordinate!.x + 1 };
-
-      return this.clone({ body: this.body.concat(newBodyItem) });
-    }
-    if (this.lastMovement === "right") {
-      const newBodyItem: Body = { y: lastCoordinate!.y, x: lastCoordinate!.x - 1 };
-      return this.clone({ body: this.body.concat(newBodyItem) });
-    }
-
     return this;
   }
 
   movePlayer(command: string, screen: Screen) {
-    const acceptedMoves: acceptedMoves = {
+    const acceptedMoves: AcceptedMoves = {
       up: (): Player => {
         const { playerY: y } = this;
         return this.move(this.clone({ y: y - 1, lastMovement: "up" }), screen);
