e56be8deb7d06f25992ba1b182b4ab34215d2d37
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2024-03-12 21:37:09 -0300

    build configuration

--- apps/backend/src/infra/main.ts
+++ apps/backend/src/infra/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from "@nestjs/core";
+import { Game, Screen } from "@snake/core";
 
 import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3333);
+
+  const game = Game.create({
+    fruits: {},
+    players: {},
+    screen: Screen.createScreen({ height: 50, width: 50 }),
+  });
 }
 bootstrap();
